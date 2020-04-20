22d454801d01cdc34275b0d59499ba3f717c6ca8
peter14067 <peter14067@gmail.com>
2020-04-20 21:37:19 +0800

    Updates

---
+++ precache-manifest.9c69ec43863ec7b3415555a57cba847b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd6068b9fae3527dc63990d5b7d13689",
+    "url": "/test2/index.html"
+  },
+  {
+    "revision": "0ab7f18f87d64f4336a9",
+    "url": "/test2/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "ea4e476061560d96cf6e",
+    "url": "/test2/static/css/main.748dbfec.chunk.css"
+  },
+  {
+    "revision": "0ab7f18f87d64f4336a9",
+    "url": "/test2/static/js/2.1f683c1f.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/test2/static/js/2.1f683c1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea4e476061560d96cf6e",
+    "url": "/test2/static/js/main.882be7b7.chunk.js"
+  },
+  {
+    "revision": "defa3562a07377a1e50a",
+    "url": "/test2/static/js/runtime-main.62bdc96e.js"
+  }
+]);
